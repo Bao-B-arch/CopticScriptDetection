fixing features computation and adding features selection

compute_features.py
@@ -1,22 +1,31 @@
 import numpy as np
 import pandas as pd
-from sklearn.feature_extraction import image
 
 def mean_grayscale(database, size):
     df_means = pd.DataFrame(
         columns=["Letter"]
     )
+    nb_pairs = 28//(size//2)
+    print(f"Computing {(nb_pairs - 1)*(nb_pairs - 1)} patches for each image")
+    arr = np.linspace(0, 28, nb_pairs + 1)
 
     for folder, imgs in database.items():
         for img in imgs:
-            patches = image.extract_patches_2d(img, (size, size))
-            patches = patches[::size//2]
-
+            patch_idx = 0
             row = pd.DataFrame({"Letter": [folder]})
 
-            for idx, patch in enumerate(patches):
-                mean = np.mean(patch)
-                row.loc[:, f"patch_{idx}"] = mean
+            for i in range(nb_pairs - 1):
+                col_l = int(arr[i])
+                col_h = int(arr[i + 2])
+
+                for j in range(nb_pairs - 1):
+                    row_l = int(arr[j])
+                    row_h = int(arr[j + 2])
+
+                    patch = img[row_l:row_h, col_l:col_h]
+                    mean = np.mean(patch)
+                    row.loc[:, f"patch_{patch_idx}"] = mean
+                    patch_idx += 1
 
             df_means = pd.concat([df_means, row], ignore_index=True)
 

main.py
@@ -12,8 +12,8 @@ from sklearn.svm import SVC
 
 # Importation des outils de validation
 from sklearn.metrics import accuracy_score, matthews_corrcoef, confusion_matrix
-from sklearn.model_selection import train_test_split, StratifiedKFold
-from sklearn.feature_selection import RFECV
+from sklearn.model_selection import train_test_split
+from sklearn.feature_selection import SelectKBest
 
 # Importation du module de chargement des données
 import data_loading
@@ -33,7 +33,9 @@ if __name__ == "__main__":
     raw_data = data_loading.load_database(DATABASE_PATH)
 
     # Calcul de la moyenne des niveaux de gris des images
-    means_data = compute_features.mean_grayscale(raw_data, 18)
+    print("FEATURES COMPUTATION:")
+    means_data = compute_features.mean_grayscale(raw_data, 10)
+    print("-"*NUMBER_SECTION_DEL)
 
     # Nettoyage des données
     # Suppression des valeurs manquantes
@@ -50,12 +52,11 @@ if __name__ == "__main__":
 
     # Définition des caractéristiques (features) utilisées pour la classification
     X = means_data.loc[:, means_data.columns != "Letter"]
+    selector = SelectKBest(k=8)
+    selector.fit(X, y)
 
-    # estimator = RandomForestClassifier(random_state=RANDOM_STATE, max_depth=10)
-    # selector = RFECV(estimator=estimator, step=1, cv=StratifiedKFold(2), scoring="accuracy")
-    # selector.fit(X, y)
-    # print(selector.n_features_)
-    means_features = means_data.columns[means_data.columns != "Letter"]
+    X = X.loc[:, selector.get_feature_names_out()]
+    means_features = X.columns
 
     print(
         f"FEATURE NAME:\n\
@@ -102,14 +103,6 @@ TEST SIZE:\t\t{len(test_X)} | POURCENTAGE:{(len(test_X) / len(X) * 100):.2f}%\n\
     ], axis=1, keys=["train", "test"])
     df_set.plot(kind="bar", stacked=True, color=["steelblue", "red"])
     plt.title("Number of letters in each dataset")
-
-    # C_range = np.logspace(-2, 10, 1)
-    # gamma_range = np.logspace(-9, 3, 2)
-    # param_grid = {"gamma": gamma_range, "C": C_range}
-    # cv = StratifiedShuffleSplit(n_splits=3, test_size=0.2, random_state=RANDOM_STATE)
-    # grid = GridSearchCV(SVC(), param_grid=param_grid, cv=cv, verbose=True)
-    # grid.fit(X, y)
-    # print(f"The best parameters are {grid.best_params_} with a score of {grid.best_score_:.2f}")
 
     # Définition et initialisation du modèle de classification Random Forest
     rfc = RandomForestClassifier(random_state=RANDOM_STATE, class_weight="balanced")
@@ -175,4 +168,3 @@ TEST SIZE:\t\t{len(test_X)} | POURCENTAGE:{(len(test_X) / len(X) * 100):.2f}%\n\
     ## Améliorer les classifier
     ## Implémenter une approche par Bootstrap pour la validation
     ## Exporter les features pour présentation et gain de temps lors du calcul
-    ## Features selection